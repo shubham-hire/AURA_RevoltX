8f2846f3f6c8fa138b2d7e402efdcd6073aeb32c
Shubham Hire <shubham.hire2006@gmail.com>
2025-09-24 21:15:01 +0530

    initial commit

---
+++ public/Readme.md
@@ -0,0 +1,30 @@
+# To run the backend server 
+
+    step1: 
+    
+        cd ~/Desktop/AURA_MAIN
+
+    step2 : 
+    
+        node server.js
+
+    step3 : 
+        <!-- Paste this in your browser -->
+        http://localhost:3000/index.html
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ public/script.js
@@ -0,0 +1,25 @@
+function redirecttologin() {
+    let img = document.getElementById("login")
+    window.location.href = "loginpage2.html";
+}
+
+
+function redirecttocart() {
+    window.open("cart.html", "_blank");
+}
+
+
+function redirecttoregister() {
+    let div = document.getElementById("ToRegister");
+    window.location.href = "register.html";
+}
+
+function GoBacktoHome() {
+    window.location.href = "index.html";
+}
+
+function redirecttocheckout() {
+    let img = document.getElementById("checkout-btn")
+    window.location.href = "checkout.html";
+
+}
