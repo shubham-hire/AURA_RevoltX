6285979a71f709cc553da7ab200b4b54de1f3fb5
Shubham Hire <shubham.hire2006@gmail.com>
2025-10-19 20:39:46 +0530

    updates

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const app = express();
 const PORT = 3000;
 
 // Serve static files from the "public" folder
+//this is the path to public folder
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(express.json());
